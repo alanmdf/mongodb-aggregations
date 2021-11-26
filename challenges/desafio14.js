553222586fe968bb88dc94357a77ff53377a522d
alanmdf <alanmoreirascr@gmail.com>
2021-11-26 17:08:27 -0300

    Requisito 14 finalizado

---
+++ challenges/desafio14.js
@@ -0,0 +1,29 @@
+// db.trips.find();
+
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracaoViagem: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60] } },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: "$duracaoViagem" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+]);
